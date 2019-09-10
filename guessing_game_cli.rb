6056ab3e39a1cd2dee2c92ca71bc8424ecfbd8f7
Solomon R Pena <solomonraulpena@gmail.com>
2019-09-10 00:55:51 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -16,11 +16,11 @@
 #   end
 # end
 
-def run_guessing_game
-input=gets.chomp
-puts "Goodbye!"
-input
-puts "Sorry! The computer guessed 6."
-input
-puts "You guessed the correct number!"
-end
+# def run_guessing_game
+# input=gets.chomp
+# puts "Goodbye!"
+# input
+# puts "Sorry! The computer guessed 6."
+# input
+# puts "You guessed the correct number!"
+# end

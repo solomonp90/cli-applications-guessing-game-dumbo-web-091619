e5eb12c8fe2efc1a9d290c39b837f32507cbefad
Solomon R Pena <solomonraulpena@gmail.com>
2019-09-10 01:21:23 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -5,10 +5,10 @@
 def run_guessing_game
   puts "What's your guess?"
   num = rand(1..6) + 1
-  response = gets.strip
+  # response = gets.strip
   if response.to_i == num
     puts "You guessed the correct number!"
-  # elsif response == "exit"
+  elsif response == "exit"
     puts "Goodbye!"
   else
     puts "Sorry! The computer guessed #{num}."
